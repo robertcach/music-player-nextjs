ea793e988f72edbaa40b83faeff584da8eeffc95
Robert Cabrera <cabrera.rch@gmail.com>
2023-04-04 19:06:20 +0200

    Add NavBar and main content to Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import NavBar from '@/components/NavBar/NavBar';
+
 export default function Home() {
   return (
-    <main className="bg-black w-full h-full">
-      <p className="text-white">Music player</p>
-    </main>
+    <div className="flex">
+      <header className="w-2/12">
+        <NavBar />
+      </header>
+      <main className="bg-[#1F1F22] h-screen w-10/12">
+        <p className="text-white">Music player</p>
+      </main>
+    </div>
   );
 }
