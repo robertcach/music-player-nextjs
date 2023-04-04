09e7bfd956fd8167ac46a50f3158539f1c5d9e12
Robert Cabrera <cabrera.rch@gmail.com>
2023-04-04 19:03:10 +0200

    Create interfaces and constant files

---
+++ app/constants.ts
@@ -0,0 +1,12 @@
+import { NavLink } from '../interfaces/general';
+
+export const NAV_LINKS: NavLink[] = [
+  {
+    label: 'Home',
+    route: '/',
+  },
+  {
+    label: 'Search',
+    route: '/search',
+  },
+];
